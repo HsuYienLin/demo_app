cebc82f0e94fbc7cccf0938ac5272dd8ae161c8e
Hsuyienlin <xyl.59@163.com>
2018-12-01 23:44:54 +0800

    finish demo_app

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,40 @@
+class MicropostsController < ApplicationController
+
+
+
+	def index
+
+
+
+	end
+
+	def show
+
+
+
+	end
+
+	def new
+
+
+
+	end
+
+	def create
+
+
+
+	end
+
+	def update
+
+
+	end
+
+	def destroy
+
+
+
+	end
+end
+
